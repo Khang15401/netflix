70a50a9a9500eb22970ffd9f57b0bd7e3d5ca334
Khang <khangb1910234@student.ctu.edu.vn>
2023-01-11 16:18:20 +0700

    featured

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
@@ -6,65 +7,8 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
-      <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhddasdasdasdasdshdkjashdkjashdkjashdkjasashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhdsdas
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasdsadajhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhdasddddddddddddddddddddddddddddddddddddd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
-    </div>
+      <Featured />
+      </div>
   )
 }
 
