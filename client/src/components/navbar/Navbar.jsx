9c8d1c795a3647eea689f11e1690c64d1fae530d
Khang <khangb1910234@student.ctu.edu.vn>
2023-01-11 15:19:16 +0700

    first commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import "./navbar.scss";
+// import logo from "../../assets/images/logo1.png"
+// import logo1 from "../../assets/images/Thank.png"
+// import logo2 from "../../assets/images/LOGO2.png";
+import { Search, Notifications, ArrowDropDown } from "@mui/icons-material";
+import { useState } from "react";
+
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+      return () => (window.onscroll = null);
+    };
+
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" 
+            />
+          {/* <img src={logo2} /> */}
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon"/>
+          <span>KID</span>
+          <Notifications className="icon"/>
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            />
+            <div className="profile">
+              <ArrowDropDown className="icon" />
+              <div className="options">
+                  <span>Cài đặt</span>
+                  <span>Logout</span>
+              </div>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,71 @@
+import Navbar from "../components/navbar/Navbar";
+import "./home.scss";
+
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhddasdasdasdasdshdkjashdkjashdkjashdkjasashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjasasdhdkjashdkjashdkjashdkjashdkajshdkasjhddddddasdasdasdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhdsdas
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasdsadajhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhdasddddddddddddddddddddddddddddddddddddd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+      asjkhdaskjdhaskjdhaskdjashdkjashdkjashdkjashdkjashdkjashdkjashdkjashdkajshdkasjhd
+    </div>
+  )
+}
+
+export default Home
