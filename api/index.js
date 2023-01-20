a5597a61e2252a3a6cf302c152643f69cc3d911a
Khang <khangb1910234@student.ctu.edu.vn>
2023-01-20 10:52:57 +0700

    register

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+mongoose.set("strictQuery", true);
 
 dotenv.config();
 
@@ -9,9 +11,14 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useCreateIndex: true,
   })
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running");
